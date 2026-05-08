b5553e93612b6d052f51eb70c95bc2213ee5fd25
oneclicktechy <oneclicktechy@gmail.com>
2026-05-08 11:34:39 +0530

    feat: implement website development service pages and add Footer and Navbar components

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,10 +4,13 @@ import ProductsSection from "./ourProducts";
 
 function Page() {
     return (
-        <main className="overflow-hidden pt-16 md:pt-20">
-            <ProductsSection />
-            <CTA />
-            <FAQSection />
+        // <main className="overflow-hidden pt-16 md:pt-20">
+        //     <ProductsSection />
+        //     <CTA />
+        //     <FAQSection />
+        // </main>
+         <main className="overflow-hidden pt-16 md:pt-20 min-h-screen w-full bg-black">
+             
         </main>
     )
 }
